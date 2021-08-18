fb06839f986fbe179e59935c4c05b916144c1bf7
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2021-08-18 11:07:49 +0100

    Add files via upload
    Assignment # 10
    Assignment # 11
    Assignment # 12

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<script>
+alert (" Is first time when i use JavaScript programming.") ;
+function Basic_JavaScript_1() }
+var string = "This will be nice lesson";
+var results = string.font-color ("purple") ; 
+document.getElementById ("Welcome").innerHTML =
+result ; 
+}
+</script>
+</html>
---
+++ new.js
@@ -0,0 +1,6 @@
+<!doctype html>
+<html>
+<script>
+alert = (" Is first time when i use JavaScript programming.") ;
+</script>
+</html>
